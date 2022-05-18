000546aca69f8633c7342bffb28bc9b4012f3116
AlexKsenych <alex.ksenych04@gmail.com>
2022-05-18 13:30:33 +0300

    added style Content.sass

--- src/components/App/index.js
+++ src/components/App/index.js
@@ -1,10 +1,12 @@
 import './App.sass';
 import Header from '../Header';
+import Content from '../Content';
 
 function App() {
   return (
     <div className='app'>
       <Header/>
+      <Content/>
     </div>
   );
 }
--- src/components/Content/index.js
+++ src/components/Content/index.js
@@ -1,15 +1,16 @@
 import React from 'react'
+import './Content.sass';
 
 const Content = () => {
   return (
     <main className='content'>
-        <div className="content__item">
-            <img src="" alt="dadadad" className="content__item__img" />
+        {/* <div className="content__item">
+            <img src="https://townsquare.media/site/442/files/2020/11/pred-11223344.jpg?w=980&q=75" alt="dadadad" className="content__item__img" />
             <div className="content__item__descr">
                 <p className="content__item__title">Predator</p>
                 <p className="content__item__rating">7.80</p>
             </div>
-        </div>
+        </div> */}
     </main>
   )
 }
